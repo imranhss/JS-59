f50c197736fe77a55ac1dbe184d1b877aee615a9
Muhammad Emran Hossain <emranhss@gmail.com>
2024-03-06 18:59:00 +0600

    Add

--- Form/form.js
+++ Form/form.js
@@ -6,22 +6,31 @@ function submitForm(event) {
     let rName = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-
     let gender=document.querySelector('input[name="gender"]:checked').value;
+    let hobby=document.querySelectorAll('input[name="hobby"]:checked');
+    let course=document.getElementById("course").value;
+    let dob=document.getElementById("dob").value;
+    let address=document.getElementById("address").value;
 
-   
-    
+    let hoobyValue=[];
 
+    hobby.forEach(
+        function(hobby){
 
+            hoobyValue.push(hobby.value); 
+        }
+    );
 
 
 
     let output = "Name: " + rName + "\n";
-
     output += "Email: " + email + "\n";
-
     output += "Password: " + password + "\n";
     output += "Gender: " + gender + "\n";
+    output += "Hobby: " + hoobyValue + "\n";
+    output += "Course: " + course + "\n";
+    output += "DoB: " + dob + "\n";
+    output += "Address: " + address + "\n";
 
 
     let newWindow = window.open("", '_blank');
@@ -32,5 +41,4 @@ function submitForm(event) {
 
 
 let myForm = document.getElementById('myForm');
-
 myForm.addEventListener('submit', submitForm);
