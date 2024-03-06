6d71d0b73a9310f4526b6c8060ca753fcb3793cb
Muhammad Emran Hossain <emranhss@gmail.com>
2024-03-06 17:02:36 +0600

    Add JS

--- Form/form.js
+++ Form/form.js
@@ -1,11 +1,28 @@
 
+function submitForm(event) {
 
-let name=document.getElementById('name').value;
-let email=document.getElementById('email').value;
-let password=document.getElementById('password').value;
+    event.preventDefault();
+
+    let rName = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
 
 
 
+    let output = "Name: " + rName + "\n";
+
+    output += "Email: " + email + "\n";
+
+    output += "Password: " + password + "\n";
 
 
+    let newWindow = window.open("", '_blank');
+    newWindow.document.write("<pre>" + output + "</pre>");
 
+
+}
+
+
+let myForm = document.getElementById('myForm');
+
+myForm.addEventListener('submit', submitForm);
